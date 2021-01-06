d2b088697de93d346235785e4b52f9fa941652d5
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-01-06 15:32:09 +0700

    first

---
+++ src/main/java/id/ten/app/democacheaside/DemoCacheAsideApplication.java
@@ -0,0 +1,13 @@
+package id.ten.app.democacheaside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCacheAsideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCacheAsideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ten/app/democacheaside/DemoCacheAsideApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ten.app.democacheaside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCacheAsideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
